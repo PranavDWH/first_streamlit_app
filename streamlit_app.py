d1493746d3d04c8f8110e7f31f54a64b2f6f0d0f
PranavDWH <135611047+PranavDWH@users.noreply.github.com>
2023-06-05 20:43:25 +0530

    Update streamlit_app.py
    Added Food Icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Heathly Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Idli Sambhar')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥣 Idli Sambhar')
+
+  
