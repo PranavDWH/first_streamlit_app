8debbc145ec9828901e2a1d7ff725b9ddae95db4
PranavDWH <135611047+PranavDWH@users.noreply.github.com>
2023-06-05 17:20:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Heathly Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Idli Sambhar')
