fdd45b0f7abbf02400b7eab5b084202cdeacb45b
PranavDWH <135611047+PranavDWH@users.noreply.github.com>
2023-06-29 22:31:32 +0530

    Update streamlit_app_v2.py

--- streamlit_app_v2.py
+++ streamlit_app_v2.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Heathly Dinner')
 streamlit.header('Breakfast Menu')
@@ -8,7 +12,7 @@ streamlit.text('🥣 Idli Sambhar')
   
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 df = streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(my_fruit_list.Fruit)
@@ -32,7 +36,7 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
 streamlit.write('The user entered', fruit_choice)
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response)  -- It will only show response(200). We will need to convert it to JSON
 # streamlit.text(fruityvice_response.json())  # writes data in json format on screen
@@ -42,7 +46,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output will come on screen in table format
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+streamlit.stop()
+
+
 
 #######
 # my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -51,6 +57,8 @@ import snowflake.connector
 # my_data_row = my_cur.fetchone()
 # streamlit.text("Hello from Snowflake:")
 # streamlit.text(my_data_row)
+
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -66,7 +74,7 @@ streamlit.dataframe(my_data_rows) # passing my_data_rows as variable to show mul
 streamlit.header("What fruit would you like to add?")
 add_my_fruit = streamlit.text_input('jackfruit')
 streamlit.write('thanks for adding ', add_my_fruit)
-
+query_stmt = 
 my_cur.execute("INSERT INTO FRUIT_LOAD_LIST VALUES ('''add_my_fruit''')")
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_rows2 = my_cur.fetchall() # fetchone function fetches only one value. Instead we will need to use fetchall()
