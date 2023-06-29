2d8f95188616ba7cce9680f0d1977b7090ed1745
PranavDWH <135611047+PranavDWH@users.noreply.github.com>
2023-06-29 21:54:10 +0530

    Update streamlit_app_v2.py

--- streamlit_app_v2.py
+++ streamlit_app_v2.py
@@ -65,6 +65,10 @@ streamlit.dataframe(my_data_rows) # passing my_data_rows as variable to show mul
 # Take suggestion of what fruit customers want to add
 streamlit.header("What fruit would you like to add?")
 add_my_fruit = streamlit.text_input('jackfruit')
-streamlit.text(add_my_fruit)
-my_data_rows = my_data_rows + add_my_fruit
-streamlit.dataframe(my_data_rows)
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("INSERT INTO FRUIT_LOAD_LIST SELECT $add_my_fruit ")
+
+# streamlit.text(add_my_fruit)
+# my_data_rows = my_data_rows + add_my_fruit
+# streamlit.dataframe(my_data_rows)
